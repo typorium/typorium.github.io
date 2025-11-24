828b00d1eaae4a732deb3724bd11e106e4df994c
typorium <lmmsmusic2@gmail.com>
2025-11-24 20:26:45 +0100

    Added home page, modified css classes, made navigaton bar js script

---
+++ js/shared/navigation_bar.js
@@ -0,0 +1,21 @@
+function insert_navigation_bar(root_relative_path)
+{
+
+    //////////////////////////////////////////////
+    // LINKS 
+    //////////////////////////////////////////////
+
+    // Logo
+    let IMG_LOGO = root_relative_path + "assets/imgs/shared/typorium_logo.png";
+
+    // Home
+    let HOME = root_relative_path + "index.html";
+    
+
+    return (
+    `<div class='navigation-bar'>` +
+        `<img class='navigation-bar-item' id='navigation-bar-logo' src=${IMG_LOGO} alt='X(' >` +
+        `<a class='navigation-bar-item' href=${HOME} >Home</a>` +
+    `</div>`
+    );
+}
